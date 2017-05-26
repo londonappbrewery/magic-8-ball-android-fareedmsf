c22ec95d9152e76d2ca91ebb56f1d5a5e0f2d2a1
fareed <shaikfareed456@gmail.com>
2017-05-26 08:10:33 +0530

    challenge completed

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button pressmebtn;
+
+        pressmebtn =(Button)findViewById(R.id.pressme);
+
+        final ImageView letterimg = (ImageView)findViewById(R.id.f);
+
+        final int [] letters = {
+            R.drawable.ff,
+            R.drawable.rr,
+            R.drawable.ee,
+            R.drawable.dd
+
+        };
+
+        pressmebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomletter =new Random();
+                 int rendom=randomletter.nextInt(4);
+                letterimg.setImageResource(letters[rendom]);
+            }
+        });
+
     }
 }
